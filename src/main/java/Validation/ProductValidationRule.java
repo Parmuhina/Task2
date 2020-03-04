8a7ba823503e9c438f662229ed6d4664d0c4b1ce
Parmuhina <60193114+Parmuhina@users.noreply.github.com>
2020-03-04 06:35:24 +0200

    Update ProductValidationRule.java

--- src/main/java/Validation/ProductValidationRule.java
+++ src/main/java/Validation/ProductValidationRule.java
@@ -4,10 +4,4 @@ import Domain.ProductVariables;
 public interface ProductValidationRule {
 
     void validate(ProductVariables productVariables);
-
-    default void checkNotNull(ProductVariables productVariables) {
-        if (productVariables == null) {
-            throw new ProductValidationException("Product must be not null");
-        }
-    }
 }
